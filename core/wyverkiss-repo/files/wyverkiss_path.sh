18a0fa62fb8e89a7b25f7abf7896e1c4926f7eda
Muhammad Herdiansyah <herdiansyah@netc.eu>
2020-02-16 17:43:46 +0700

    wyverkiss-repo: update path

--- core/wyverkiss-repo/files/wyverkiss_path.sh
+++ core/wyverkiss-repo/files/wyverkiss_path.sh
@@ -1,1 +1,1 @@
-export KISS_PATH="/var/db/kiss/wyverkiss/core:/var/db/kiss/wyverkiss/extra:/var/db/kiss/wyverkiss/gnu:$KISS_PATH"
+export KISS_PATH="/var/db/kiss/wyverkiss/core:/var/db/kiss/wyverkiss/extra:/var/db/kiss/wyverkiss/xorg:var/db/kiss/wyverkiss/gnu:$KISS_PATH"
